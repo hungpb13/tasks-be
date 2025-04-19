63d67f00f194c85aea6034f333e59a565fc1fd0f
hungpb13 <hungpb13@gmail.com>
2025-04-19 20:08:19 +0700

    Implement TaskList Entity

---
+++ src/main/java/com/dev/tasks/domain/entities/TaskList.java
@@ -0,0 +1,34 @@
+package com.dev.tasks.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "task_lists")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updated;
+}
